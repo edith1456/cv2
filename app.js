e33b35314adafac87acec4e8c797952d014f550c
Edith Awino <edithaabrooks@gmail.com>
2023-08-20 15:19:11 +0300

    few changes in cv and added projects to projects page

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-const cursorFollower = document.querySelector('#cursor-follower');
-const ballRadius = 6; // Adjust this to match the radius of your ball
+const cursorFollower = document.querySelector("#cursor-follower");
+const ballRadius = 6;
 
-document.addEventListener('mousemove', (e) => {
+document.addEventListener("mousemove", (e) => {
   const cursorX = e.clientX;
   const cursorY = e.clientY;
 
-  // Calculate the position for the ball to be centered under the cursor
   const ballX = cursorX - ballRadius;
   const ballY = cursorY - ballRadius;
 
   cursorFollower.style.transform = `translate(${ballX}px, ${ballY}px)`;
 });
-
